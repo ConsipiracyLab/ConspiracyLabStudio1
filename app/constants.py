06cb730c4cb3fb20cc9ee9cc3cbc47e4ad19ffdd
Tomoki Takasawa <>
2021-07-10 23:38:50 -0400

    initial commit

---
+++ app/constants.py
@@ -0,0 +1,8 @@
+# Decentralized network
+MAINNET = 'mainnet'
+ROPSTEN = 'ropsten'
+
+NETWORK_URLS = {
+    ROPSTEN: 'https://ropsten.infura.io/v3/'
+}
+
---
+++ app/scripts/call_buy_back_leak.py
@@ -0,0 +1,5 @@
+from app.constants import ROPSTEN, NETWORK_URLS
+
+class CallBuyBackLeak:
+    def run(self):
+        pass
